a88cd381dc48eadc8dbf5afeb1dc2144aa0117e7
jaehoon-Han <eroel1060@daum.net>
2022-05-27 10:09:35 +0900

    Update app_BMI.py

--- app_BMI.py
+++ app_BMI.py
@@ -9,9 +9,9 @@ def run_bmi() :
     gender = st.radio('성별을 선택하세요.', ['남자','여자'])
     
 
-    age = st.slider('나이', 1,100)
-    weight = int(st.slider('몸무게',30, 150))
-    height = int(st.slider('키',100,210))
+    age = st.slider('나이', 6,100,25)
+    weight = int(st.slider('몸무게',30, 150, 60))
+    height = int(st.slider('키',100,210, 170))
 
    
     if  st.button('BMI 측정하기') :
@@ -42,5 +42,10 @@ def run_bmi() :
             st.text('고강도 운동')
         else :
             st.text('놀아라')
-
+        if age >= 65 :
+            st.image('https://cdn.pixabay.com/photo/2018/07/22/05/16/person-3553814__340.jpg')
+        elif age >= 18 :
+            st.image('https://cdn.pixabay.com/photo/2018/04/05/17/21/kettlebell-3293478__340.jpg')
+        else :
+            st.image('https://cdn.pixabay.com/photo/2015/01/26/22/40/child-613199__340.jpg')
    
