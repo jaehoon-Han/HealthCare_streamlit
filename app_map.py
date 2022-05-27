8f35a0a66acd922a641a48d5e908e0b3c0b0ec0c
jaehoon-Han <eroel1060@daum.net>
2022-05-27 11:17:16 +0900

    add

--- app_map.py
+++ app_map.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import googlemaps
 def run_map() :
     df = pd.DataFrame(
         np.random.randn(1000,2) / [50, 50] + [37.76, -122.4],
         columns = ['lat', 'lon'])
 
-
-    st.map(df)  
-    st.snow()
+    gmaps_key = 'AIzaSyAC9bK-fOBY_JcKs9uhH4hiCiIv0O9aEJY'
+    gmaps = googlemaps.Client(key=gmaps_key)
+    gg = gmaps.geocode('헬스장', language='ko')
+    st.dataframe(gg)    
