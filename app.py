2998f05f6b709335c4c86b614f45b5abed866e4b
jaehoon-Han <eroel1060@daum.net>
2022-05-31 09:37:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from app_map import run_map
 def main():
 
     st.title('체력 자가진단 및 운동 추천 시스템')
-    menu = ['Home', 'BMI', 'Recommend']
+    menu = ['Home', 'BMI', 'Recommend', "Today's Workout"]
     
     choice = st.sidebar.selectbox('메뉴 선택', menu)
 
@@ -19,6 +19,8 @@ def main():
         run_bmi()
     elif choice == menu[2] :
         run_map()
+    elif choice == menu[3] :
+        pass
 
 
 
